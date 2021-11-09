0c90acddd5889667e3a4dcf6088a77fec7ab5085
Nathan Noack <nnoack3@gmail.com>
2021-11-09 16:06:50 -0500

    added more styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,25 +6,26 @@ const Header = props => {
   const navStyle = {
     display: "flex",
     justifyContent: "space-around",
+    alignItems: "center",
     backgroundColor: "black",
     padding: "0.75em",
     width: "95%",
     margin: "auto",
     fontSize: "1.25em",
-    color: "lightblue"
   };
 
   return (
     <header>
       <nav style={navStyle}>
+      <img src="https://www.testbytes.net/wp-content/uploads/2019/06/Untitled-41-300x294.png" alt="programmer meme pointing at semi-colon" width={100} />
         <Link to="/">
-          <div>HOME</div>
+          <div className="title is-4">HOME</div>
         </Link>
         <Link to="/about">
-          <div>ABOUT</div>
+          <div className="title is-4">ABOUT</div>
         </Link>
         <Link to="/projects">
-          <div>PROJECTS</div>
+          <div className="title is-4">PROJECTS</div>
         </Link>
       </nav>
     </header>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,28 @@
 const Home = props => {
+
+  const homeStyle = {
+    position: "fixed",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)"
+  }
+  const thingsStyle = {
+    display: "flex",
+    justifyContent: "center",
+
+  }
+
+
   return (
-    <div className="Home">
-      <h1>Making things that make a difference</h1>
-      <div className="things">
-        <div className="left">
-          Thing 1 - Your difference, today!
+    <div style={homeStyle}>
+      <h1 className="title is-1 mb-1">Making things that make a difference</h1>
+      <h1 className="title is-6 mb-6">(and messing around with mixed ways to style React)</h1>
+      <div className="things" style={thingsStyle}>
+        <div className="thing" style={{minWidth: "180px", width: "30%", padding: "1em", margin: "auto", border: "2px solid black"}}>
+          Thing 1 - One difference, today!
         </div>
-        <div className="right">
-          Thing 2 - A better, more improved Thing 2.0
+        <div className="thing" style={{minWidth: "180px", width: "30%", marginLeft: "2em", padding: "1em", margin: "auto", border: "2px solid black"}}>
+          Thing 2 - A better, more improved Thing 1
         </div>
       </div>
     </div>
